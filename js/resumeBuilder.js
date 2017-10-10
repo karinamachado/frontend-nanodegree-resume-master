3fa07f115c650661e7f35282e79af4c9221c496e
karinamachado <karinamachado@msn.com>
2017-10-10 16:55:29 -0300

    Update

---
+++ js/resumeBuilder.js
@@ -0,0 +1,72 @@
+
+
+$("#header").append()
+
+$("#main").append(["funThoughts"]);
+
+
+
+var awesomeThoughts = "I am Karina, I am AWESOME!"
+
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+
+
+var email="karina@womenwhocode.com";
+
+var newEmail=email.replace("womenwhocode","gmail");
+
+
+
+var formattedName = HTMLheaderName.replace(%data%, name);
+
+var role= "Web Developer";
+
+var formattedRole = HTMLheaderRole.replace(%data%,role);
+
+$("#header").prepend(formattedName);
+
+$("#header").prepend(formattedRole);
+
+
+
+var bio={
+
+	"name" : "John Doe",
+	"role" : "Web Developer",
+	"contacts":{
+		"mobile": "650-555-5555",
+		"email":"john@examole.com",
+		"github":"johndoe",
+		"twitter": "@johndoe",
+		"location": "San Francisco"
+	},
+	"welcomeMessage": "lorem ipsum dolor sit etc etc."
+	"skills":["awesomeness", "delivering things", "cryogenic sleep", "saving the universe"],
+	"biopic": "images/fry.jpg",
+}
+
+var work = {
+
+	work.position="Software Engineering";
+	work.city="Recife";
+	work.years=0.5;
+	work.employer="Ryuge.net";
+}
+
+var education = {
+
+education["name"]= "Guararapes College";
+education["years"]= "2011-2013";
+education["city"]= "Recife-PE, Brazil"
+
+}
+
+
+$("#main").append(work.["position"]);
+$("#main").append(education.name);
+
+
+
+
+ 
