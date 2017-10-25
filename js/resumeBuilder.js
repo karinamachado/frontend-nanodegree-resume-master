aea3379ae52970125c182b89275f51e5c291b79c
karinamachado <karinamachado@msn.com>
2017-10-25 15:40:45 -0300

    Update, Lesson 10
    Doing quiz lesson 10

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,68 +5,54 @@ $("#header").append()
 $("#main").append(["funThoughts"]);
 
 
-
-var awesomeThoughts = "I am Karina, I am AWESOME!"
-
-var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-
-
-
 var email="karina@womenwhocode.com";
 
 var newEmail=email.replace("womenwhocode","gmail");
 
+//console.log(email);
+//console.log(newEmail);
 
 
-var formattedName = HTMLheaderName.replace(%data%, name);
+var awesomeThoughts = "My names is Karina and I am AWESOME!";
+
+var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+
+$("#main").append(funThoughts);
+
+
+var formattedName = HTMLheaderName.replace("%data%",name);
+
+$("#header").append(formattedName);
+
 
 var role= "Web Developer";
 
-var formattedRole = HTMLheaderRole.replace(%data%,role);
-
-$("#header").prepend(formattedName);
+var formattedRole = HTMLheaderRole.replace("%data%",role);
 
 $("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
 
+if(bio.skills.length>0){
+	$("#header").append(HTMLskillsStart);
 
+	var formattedSkill = HTMLskills.replace("%data%",bio.skills[0]);
 
-var bio={
+	$("#skills").append(formattedSkill);
+	formattedSkill=HTMLskills.replace("%data",bio.skills[1]);
 
-	"name" : "John Doe",
-	"role" : "Web Developer",
-	"contacts":{
-		"mobile": "650-555-5555",
-		"email":"john@examole.com",
-		"github":"johndoe",
-		"twitter": "@johndoe",
-		"location": "San Francisco"
-	},
-	"welcomeMessage": "lorem ipsum dolor sit etc etc."
-	"skills":["awesomeness", "delivering things", "cryogenic sleep", "saving the universe"],
-	"biopic": "images/fry.jpg",
-}
+	$("#skills").append(formattedSkill);
+	formattedSkill=HTMLskills.replace("%data",bio.skills[2]);
 
-var work = {
+	$("#skills").append(formattedSkill);
+	formattedSkill=HTMLskills.replace("%data",bio.skills[3]);
 
-	work.position="Software Engineering";
-	work.city="Recife";
-	work.years=0.5;
-	work.employer="Ryuge.net";
-}
-
-var education = {
-
-education["name"]= "Guararapes College";
-education["years"]= "2011-2013";
-education["city"]= "Recife-PE, Brazil"
-
+	$("#skills").append(formattedSkill);
 }
 
 
-$("#main").append(work.["position"]);
-$("#main").append(education.name);
 
 
 
 
- 
+
+
